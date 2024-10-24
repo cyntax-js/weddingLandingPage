8a6c3db664401944015dfe12212e2afa08703119
CYNTAX <samuelify225@gmail.com>
2024-10-24 21:25:59 +0100

    update

--- src/config/core.js
+++ src/config/core.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = "http://localhost:4023";
 // export const BASE_URL = "http://172.20.10.2:4022";
-export const BASE_URL = "http://91.223.236.249:4022";
+export const BASE_URL = "https://api.sefunmiandcharles.com.ng";
+// export const BASE_URL = "http://91.223.236.249:4022";
 // export const BASE_URL = "https://backtest.egomart.org";
